2c99cc04f42a3a70942aca55127bb2e0338613a2
zxyuan <zhouxueyuan1106@163.com>
2015-12-12 12:36:48 +0000

    client json support

--- session/logincheck.php
+++ session/logincheck.php
@@ -1,57 +1,82 @@
 <?php
-function check_username_password($username, $password)
+function check_username_password($username, $password, &$ret_token)
 {
+	$ret = null;
 	$mysqli = mysqli_new();
 	$sql = "select * from manager where name='root' and password='password'";
 	$sql = "select * from users where username='$username'and password='$password'";
 	$result = mysqli_obj_query($mysqli, $sql);
 	if (is_null($result) || (1 != $result->num_rows)) {
-		return "username or password was not right";
+		$ret = "username or password was not right";
 	}
 	else {
 		require_once("./session.php");
 		update_session($username);
 		while ($row = mysqli_fetch_assoc($result)) {
 			//echo json_encode($row);
-			return "ok";
+			$token = md5(uniqid(rand()));
+			$ret_token = $token;
+			$sql="INSERT INTO `loginStatus`(`username`, `token`) VALUES ('$username', '$token')";
+			$result = mysqli_obj_query($mysqli, $sql);
+			$ret = "ok";
 			break;
 		}
 	}
 	/* free result set */
-	$result->close();
+	//$result->free();
+	$mysqli->close();
+	
+	return $ret;
 }
-	require_once("../db/conn.php");
 
-	#echo file_get_contents("php://input");
-	#echo $_POST["username"];
-    #echo $_POST["password"];
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-	if(isset($_POST["submit"]))	{
-		if($username == "") {
-			echo "<script>alert('please input your username'); history.go(-1);</script>";
-		} else if ($password == "") {
-			echo "<script>alert('please input your password'); history.go(-1);</script>";
-		} else {
-			$result = check_username_password($username, $password);
-			if ("ok" == $result) {
-				echo "<script language='javascript'>";
-				echo " location='/main/index.php';";
-				echo "</script>";
-			} else {
-				echo "<script>alert('$result'); history.go(-1);</script>";
-			}
-		}
-	} else if (isset($_POST["device"])) {
-		if($username == "") {
-			echo json_encode('please input your username');
-		} else if ($password == "") {
-			echo json_encode('please input your password');
-		} else {
-			$result = check_username_password($username, $password);
-			echo json_encode($result);
-		}
+require_once("../db/conn.php");
+
+#echo file_get_contents("php://input");
+#echo $_POST["username"];
+#echo $_POST["password"];
+$username = $_POST["username"];
+$password = $_POST["password"];
+$token = null;
+$obj = new stdClass();
+if(isset($_POST["submit"]))	{
+	if($username == "") {
+		echo "<script>alert('please input your username'); history.go(-1);</script>";
+	} else if ($password == "") {
+		echo "<script>alert('please input your password'); history.go(-1);</script>";
 	} else {
-		echo "<script>alert('submit faield'); history.go(-1);</script>";
+		$result = check_username_password($username, $password, $token);
+		if ("ok" == $result) {
+			echo "<script language='javascript'>";
+			echo " location='/main/index.php';";
+			echo "</script>";
+		} else {
+			echo "<script>alert('$result'); history.go(-1);</script>";
+		}
 	}
+} else if (isset($_POST["device"])) {
+
+	if($username == "") {
+		$obj->status = 'error';
+		$obj->errString = 'please input your username';
+	} else if ($password == "") {
+		$obj->status = 'error';
+		$obj->errString = 'please input your password';
+	} else {
+		$result = check_username_password($username, $password, $token);
+		$obj->username = $username;
+		if ($result == 'ok') {
+			$obj->status = $result;
+			$obj->token = $token;
+			$obj->errString = null;
+		} else {
+			$obj->status = 'error';
+			$obj->token = null;
+			$obj->errString = $result;
+		}
+	}
+	echo json_encode($obj);
+} else {
+	echo "<script>alert('submit faield'); history.go(-1);</script>";
+}
+
 ?>
